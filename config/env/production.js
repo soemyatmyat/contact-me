b88a70a6af587020dccbdf26f9500b0ec34cdf9b
Soe Myat Myat <soemyatmyat@protonmail.com>
2022-05-01 01:02:07 +0800

    update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -2,9 +2,9 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-const mailChimpKey="bf503794b9738f408a93d08dd0116b46-us17"
-const mailChimpAudienceId="b0f7def584"
-const mailChimpPrefix="us17"
+const mailChimpKey=process.env.mailChimpKey
+const mailChimpAudienceId=process.env.mailChimpAudienceId
+const mailChimpPrefix=process.env.mailChimpPrefix
 module.exports = {
    port: port,
    mailChimpKey: mailChimpKey,
